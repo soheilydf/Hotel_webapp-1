94d2d11cbf81e48850a7364b3972baa03746cf3b
soheilydf <97129446+soheilydf@users.noreply.github.com>
2022-01-07 17:02:18 +0330

    First commit
    This code is for the employee page.

---
+++ employee.py
@@ -0,0 +1,17 @@
+def catering1(food,room_number):
+    print("A customer form room number",room_number,"has requested",food)
+
+def catering2(food,room_number):
+    print(food,"is ready for room number",room_number)
+
+
+def housekeeping(room_number):
+    print("Room number",room_number,"has requested cleaning")
+
+
+def management(room_number,feedback,rating):
+    print("Room number",room_number,"has rated",rating,"/10\n","They said:\n",feedback)
+
+
+def maintenance(room_number,report):
+    print("room number ",room_number," has reported ",report)
